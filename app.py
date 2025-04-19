00234da2e31be87afee7eecd47a7f32772003db3
Sourav Pal <palsusanta1968@gmail.com>
2025-04-20 05:22:14 +0530

    remove unused logo display code and related function

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-
 # Define your pages
 account_page = st.Page(
     page="pages/account.py",
@@ -51,25 +50,6 @@ else:
 
 st.sidebar.title("CureCancAI")
 st.sidebar.text("Made by ByteForce")
-
-
-# Display the logo (optional)
-def get_base64_image(image_path):
-    with open(image_path, "rb") as img_file:
-        encoded = base64.b64encode(img_file.read()).decode()
-    return f"data:image/png;base64,{encoded}"
-
-image_base64 = get_base64_image("assets/logo.png")
-
-st.sidebar.markdown(
-    f"""
-    <a href="/" target="_self">
-        <img src="{image_base64}" width="80"  alt="logo">
-    </a>
-    """,
-    unsafe_allow_html=True
-)
-
 
 # Run Navigation
 pg.run()
